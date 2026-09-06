5992be1d355ee10df333a389df6b5f7ed26d184d
flowerismine <muhammadaryaa24@gmail.com>
2026-09-06 13:42:33 +0700

    Revert database from PostgreSQL back to MySQL

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 // Development: pakai host/user/pass/dbname terpisah
 const db = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL, {
-      dialect: "postgres",
+      dialect: "mysql",
       dialectOptions: {
         ssl: { require: true, rejectUnauthorized: false }
       },
@@ -17,7 +17,7 @@ const db = process.env.DB_URL
       process.env.DB_PASS || "",
       {
         host: process.env.DB_HOST || "localhost",
-        dialect: "postgres",
+        dialect: "mysql",
         logging: false,
       }
     );
